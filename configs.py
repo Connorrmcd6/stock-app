ef87ff8720b0fe77aa80d258ef855da0185987f0
Connor McDonald <connormcd98@gmail.com>
2023-07-16 19:29:44 +0200

    change destination files

--- configs.py
+++ configs.py
@@ -1,13 +1,13 @@
-#google configs
-path_to_json = './service_account_configs/sinani-inventory-app-f8383390239a.json' #path to service account keys
-parent_folder_key = '15JgLeHFBsHwRB6vGdCp1pnZaI7W_XpBz' # folder where invoice IMAGES are stored
-sheet_key = '1yyLl9NVVljNXrKg4Kjr_0hEum1Bv0B55x5Bgs2m1_tM' #key of sheet which you are uploading to
-sheet_name = 'Stock in Demo'
+# google configs
+# path to service account keys
+path_to_json = './service_account_configs/sinani-inventory-app-f8383390239a.json'
+# folder where invoice IMAGES are stored
+parent_folder_key = '1fAn9kx280I1sWftzGltNq8a_WB6y36_PvLPGbfN-Zz5IP-BtINowmCWstbXb6Sm0ZKQQkcQm'
+# key of sheet which you are uploading to
+sheet_key = '1XdljCZyE4NpHjr5AIsEzTFM-EAaB6r8OGi_MYsUqT4g'
+sheet_name = 'Stock In:'
 
 #stock in configs
 path_to_names = './stock_in_configs/names.csv'
 path_to_suppliers = './stock_in_configs/suppliers.csv'
 path_to_stock_units = './stock_in_configs/stock_units.csv'
-
-
-
