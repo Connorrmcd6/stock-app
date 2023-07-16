5ef21051230e6e96b9097b73f85e1f8f2ed8ff46
Connor McDonald <connormcd98@gmail.com>
2023-07-16 11:27:19 +0200

    update configs and readme

--- configs.py
+++ configs.py
@@ -1,7 +1,13 @@
-path_to_json = './service_account_configs/sinani-inventory-app-f8383390239a.json'
+#google configs
+path_to_json = './service_account_configs/sinani-inventory-app-f8383390239a.json' #path to service account keys
+parent_folder_key = '15JgLeHFBsHwRB6vGdCp1pnZaI7W_XpBz' # folder where invoice IMAGES are stored
 sheet_key = '1yyLl9NVVljNXrKg4Kjr_0hEum1Bv0B55x5Bgs2m1_tM' #key of sheet which you are uploading to
 sheet_name = 'Stock in Demo'
+
+#stock in configs
 path_to_names = './stock_in_configs/names.csv'
 path_to_suppliers = './stock_in_configs/suppliers.csv'
 path_to_stock_units = './stock_in_configs/stock_units.csv'
-parent_folder_key = '15JgLeHFBsHwRB6vGdCp1pnZaI7W_XpBz' # folder where invoice images are stored
+
+
+
